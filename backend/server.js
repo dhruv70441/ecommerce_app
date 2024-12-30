c8507542add91eeb21232657ecc4b8799ef1fe81
Dhruv Parmar <dhruvparmar70441@gmail.com>
2024-12-30 09:24:42 +0530

    added vercel.json

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,5 @@ app.get('/', (req,res) => {
 })
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`server is running on port ${process.env.PORT}`);
-    
+    console.log(`server is running on port ${process.env.PORT}`);  
 })
